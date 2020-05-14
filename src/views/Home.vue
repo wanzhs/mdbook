e1faccc7eb951c87ca60bfadfd56ec75fc421aee
wanzhs <wanzhs_cedar@163.com>
2020-05-14 10:39:24 +0800

    vue-markdown-loader+hyperdown展示markdown文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="home">
-   <HelloWorld msg="你好，helloworld"/>
-  </div>
+    <div class="home">
+        <!--   <HelloWorld msg="你好，helloworld"/>-->
+        <my-mark-down/>
+    </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue";
+    import {Component, Vue} from "vue-property-decorator";
+    import HelloWorld from "@/components/HelloWorld.vue";
+    //@ts-ignore
+    import myMarkDown from '@/md/cpu.md'
 
-@Component({components:{HelloWorld}})
-export default class Home extends Vue{
+    @Component({components: {HelloWorld, myMarkDown}})
+    export default class Home extends Vue {
 
-}
+    }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,27 @@
-module.exports={
-  css:{
-      loaderOptions:{
-          less:{
-              javascriptEnabled: true,
-          }
-      }
-  }
+let HyperDown = require('hyperdown');
+let parser = new HyperDown;
+
+module.exports = {
+    css: {
+        loaderOptions: {
+            less: {
+                javascriptEnabled: true,
+            }
+        }
+    },
+    chainWebpack: config => {
+        config.module.rule('md')
+            .test(/\.md/)
+            .use('vue-loader')
+            .loader('vue-loader')
+            .end()
+            .use('vue-markdown-loader')
+            .loader('vue-markdown-loader/lib/markdown-compiler')
+            .options({
+                raw: true,
+                preprocess: function (markdownIt, source) {
+                    return parser.makeHtml(source);//重点在这里！！！
+                }
+            })
+    }
 };
